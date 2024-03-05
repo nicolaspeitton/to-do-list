611d9a5eb272180d5402ab90046babf7de0a9876
Nicolas Peitton <nicolaspeitton@gmail.com>
2024-03-05 20:10:50 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import './App.css';
+import Header from './components/Header';
+import Form from './components/Form';
+import TodosList from './components/TodosList';
+
+const App = () => {
+
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  return (
+    <div className="container">
+      <div className="app-wrapper">
+        <div>
+          <Header></Header>
+        </div>
+        <div>
+          <Form
+            input={input}
+            setInput={setInput}
+            todos={todos}
+            setTodos={setTodos}
+            />
+        </div>
+        <div>
+          <TodosList todos={todos} setTodos={setTodos}/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
